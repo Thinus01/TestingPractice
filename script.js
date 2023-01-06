999380212430f4780590c8e8733bccbcf23984c5
Thinus <thinusvdv2001@gmail.com>
2023-01-07 00:34:17 +0200

    Add all files

---
+++ script.js
@@ -0,0 +1,9 @@
+function stringLength(string) {
+    let totalLength = 0;
+    for (let i = 0; i <= string.length; i++) {
+        totalLength = i;
+    }
+    console.log(totalLength);
+    return totalLength;
+}
+module.exports = stringLength("appel")
---
+++ script.test.js
@@ -0,0 +1,8 @@
+const { default: expect } = require('expect')
+const stringLength = require('./script')
+
+test('Determine character amount in the word appel', () => {
+    expect(
+        
+    )
+})
